255f3aaf7f8952dc76af7a7b6bea705c6480dc57
Gordon <gordlonious@gmail.com>
2018-03-01 22:00:00 -0700

    compiling Autocomplete class with GUI constructor call (gui isn't visible yet

--- autocomplete/Autocomplete.java
+++ autocomplete/Autocomplete.java
@@ -5,28 +5,41 @@
  */
 package autocomplete;
 
+import java.util.Arrays;
+
 /**
  *
  * @author gordl
  */
 public class Autocomplete {
+    private Term[] t;
     
     // Initialize the data structure from the given array of terms.
     public Autocomplete(Term[] terms) {
-        
+        t = terms;
     }
     
     // Return all terms that start with the given prefix, in descending order of weight.
-//    public Term[] allMatches(String prefix) { }
+    public Term[] allMatches(String prefix) { 
+        int fIndex = BinarySearchDeluxe.firstIndexOf(t, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
+        int lIndex = BinarySearchDeluxe.lastIndexOf(t, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
+        return Arrays.copyOfRange(t, fIndex, lIndex);
+    }
     // Return the number of terms that start with the given prefix.
-//    public int numberOfMatches(String prefix) { }
+    public int numberOfMatches(String prefix) { 
+        int fIndex = BinarySearchDeluxe.firstIndexOf(t, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
+        int lIndex = BinarySearchDeluxe.lastIndexOf(t, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
+        return (lIndex - fIndex + 1);
+    }
 
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String filename = "";
+        int numberOfSuggestions = 10;
+        AutocompleteGUI ag = new AutocompleteGUI("wiktionary.txt", numberOfSuggestions);
     }
     
 }
